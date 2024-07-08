24ff50230cb5a936ed834e5a572e3db77bae2530
aniket <aniket.umbc@gmail.com>
2024-07-08 17:02:30 +0530

    Added navigation for mobile and desktop

--- components/ui/web-components/Header.jsx
+++ components/ui/web-components/Header.jsx
@@ -1,5 +1,33 @@
+import Link from 'next/link';
+import { Button } from '../button';
+import Nav from './Nav';
+
 const Header = () => {
-  return <header>header</header>;
+  return (
+    <header className='py-8 xl:py-12 text-white'>
+      {/* Logo*/}
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link href='/'>
+          <h1 className='text-4xl font-semibold'>
+            Aniket<span className='text-accent'>.</span>
+          </h1>
+        </Link>
+        {/* 
+          Desktop nav & hire me button
+        */}
+        <div className='hidden xl:flex items-center gap-8'>
+          <Nav />
+          <Link href='/contact'>
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav */}
+
+        <div className='xl:hidden'>Mobile Nav</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
