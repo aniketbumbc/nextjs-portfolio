d4fa12e305aa3e6057766a4cee0267bed8cb932c
aniket <aniket.umbc@gmail.com>
2024-07-10 09:38:10 +0530

    button style change

--- components/ui/web-components/MobileNav.jsx
+++ components/ui/web-components/MobileNav.jsx
@@ -3,7 +3,7 @@
 import { Sheet, SheetTrigger, SheetContent } from '../sheet';
 
 import { usePathname } from 'next/navigation';
-import { Link } from 'next/link';
+import Link from 'next/link';
 import { CiMenuFries } from 'react-icons/ci';
 
 const links = [
@@ -27,10 +27,6 @@ const links = [
     name: 'contact',
     path: '/contact',
   },
-  {
-    name: 'home',
-    path: '/',
-  },
 ];
 
 const MobileNav = () => {
@@ -40,7 +36,33 @@ const MobileNav = () => {
       <SheetTrigger className='flex justify-center items-center'>
         <CiMenuFries className='text-[32px] text-accent' />
       </SheetTrigger>
-      <SheetContent>content</SheetContent>
+      <SheetContent className='flex flex-col'>
+        <div className='mt-32 mb-40 text-center text-2xl'>
+          <Link href='/'>
+            <h1 className='text-4xl font-semibold'>
+              Aniket <span className='text-accent'>.</span>
+            </h1>
+          </Link>
+        </div>
+
+        <nav className='flex flex-col justify-center items-center gap-8'>
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathName &&
+                  'text-accent border-b-2 border-accent'
+                }
+                  text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
     </Sheet>
   );
 };
