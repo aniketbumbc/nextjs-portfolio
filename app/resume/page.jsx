45cc57c560e18d1cd17cac960b39a2077980d954
aniket <aniket.umbc@gmail.com>
2024-07-12 07:12:12 +0530

    Resume section complete

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -22,7 +22,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from '../../components/ui/tabs';
+} from '../../components/ui/tooltip';
 import { ScrollArea } from '../../components/ui/scroll-area';
 import { motion } from 'framer-motion';
 
@@ -196,10 +196,10 @@ const Resume = () => {
                       return (
                         <li
                           key={index}
-                          className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'
+                          className='bg-[#232329] h-[184px] py-6 px-8 rounded-xl flex flex-col justify-center items-center lg:items-start'
                         >
                           <span className='text-accent'>{item.duration}</span>
-                          <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>
+                          <h3 className='max-w-[260px] min-h-[20px] text-center lg:text-left'>
                             {item.position}
                           </h3>
                           <div className='flex items-center gap-3'>
@@ -226,10 +226,10 @@ const Resume = () => {
                       return (
                         <li
                           key={index}
-                          className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'
+                          className='bg-[#232329] h-[184px] py-6 px-8 rounded-xl flex flex-col justify-center items-center lg:items-start'
                         >
                           <span className='text-accent'>{item.duration}</span>
-                          <h3 className='text-xl max-w-[260px] min-h-[60px] text-center lg:text-left'>
+                          <h3 className='max-w-[260px] min-h-[30px] text-center lg:text-left'>
                             {item.degree}
                           </h3>
                           <div className='flex items-center gap-3'>
@@ -241,6 +241,63 @@ const Resume = () => {
                     })}
                   </ul>
                 </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value='skills' className='w-full h-full'>
+              <div className='flex flex-col gap-[30px]'>
+                <div className='flex flex-col gap-[30px] text-center xl:text-left'>
+                  <h3 className='text-4xl font-bold'>{skills.title}</h3>
+                  <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>
+                    {skills.description}
+                  </p>
+                </div>
+
+                <ul className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]'>
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delay={100}>
+                          <Tooltip>
+                            <TooltipTrigger className='w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group'>
+                              <div className='text-6xl group-hover:text-accent transition-all duration-300'>
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className='capitalize'>{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+
+            <TabsContent
+              value='about'
+              className='w-full text-center xl:text-left'
+            >
+              <div className='flex flex-col gap-[30px]'>
+                <h3 className='text-4xl font-bold'>{about.title}</h3>
+                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>
+                  {about.description}
+                </p>
+                <ul className='grid  max-w-[520px] mx-auto xl:mx-0'>
+                  {about.info.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className='flex items-center justify-center xl:justify-start gap-4'
+                      >
+                        <span className='text-white/60'>{item.filedName}</span>
+                        <span className='text-accent'>{item.filedValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
               </div>
             </TabsContent>
           </div>
