02043268dde094394c31d1dd7f4038bd00f4a1a3
aniket <aniket.umbc@gmail.com>
2024-07-18 13:07:19 +0530

    added state for contact form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -13,6 +13,7 @@ import {
   SelectValue,
 } from '../../components/ui/select';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from 'react-icons/fa';
 
 const info = [
@@ -35,6 +36,29 @@ const info = [
 ];
 
 const Contact = () => {
+  const tempEmailData = {
+    fname: '',
+    lname: '',
+    email: '',
+    phone: '',
+    selectedService: '',
+    textAreaText: '',
+  };
+
+  const [mailData, setMailData] = useState(tempEmailData);
+
+  const sendEmail = (e) => {
+    console.log(e);
+    e.preventDefault();
+    console.log('send Email');
+    console.log(mailData);
+  };
+
+  const onSelectChange = (e) => {
+    console.log(e);
+    setMailData({ ...mailData, selectedService: e.target.value });
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -56,18 +80,42 @@ const Contact = () => {
               </p>
               {/* input */}
               <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                <Input type='fname' placeholder='First Name' />
-                <Input type='lname' placeholder='Last Name' />
-                <Input type='email' placeholder='Email Address' />
-                <Input type='Phone' placeholder='Phone Number' />
+                <Input
+                  type='fname'
+                  placeholder='First Name'
+                  onChange={(e) =>
+                    setMailData({ ...mailData, fname: e.target.value })
+                  }
+                />
+                <Input
+                  type='lname'
+                  placeholder='Last Name'
+                  onChange={(e) =>
+                    setMailData({ ...mailData, lname: e.target.value })
+                  }
+                />
+                <Input
+                  type='email'
+                  placeholder='Email Address'
+                  onChange={(e) =>
+                    setMailData({ ...mailData, email: e.target.value })
+                  }
+                />
+                <Input
+                  type='Phone'
+                  placeholder='Phone Number'
+                  onChange={(e) =>
+                    setMailData({ ...mailData, phone: e.target.value })
+                  }
+                />
               </div>
-              <Select>
+              <Select onChange={onSelectChange}>
                 <SelectTrigger className='w-full'>
                   <SelectValue placeholder='Select a service' />
                 </SelectTrigger>
 
-                <SelectContent>
-                  <SelectGroup>
+                <SelectContent onChange={onSelectChange}>
+                  <SelectGroup onChange={onSelectChange}>
                     <SelectLabel>Select a service</SelectLabel>
                     <SelectItem value='est'>Web Development</SelectItem>
                     <SelectItem value='cst'>UX/UI Design</SelectItem>
@@ -81,8 +129,11 @@ const Contact = () => {
               <Textarea
                 className='h-[200px]'
                 placeholder='Type your message here'
+                onChange={(e) => {
+                  setMailData({ ...mailData, textAreaText: e.target.value });
+                }}
               />
-              <Button size='md' className='max-w-40 h-10'>
+              <Button size='md' className='max-w-40 h-10' onClick={sendEmail}>
                 Send message
               </Button>
             </form>
